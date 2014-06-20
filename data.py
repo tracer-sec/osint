5b1706805c813aa99adfaca657a000aacc0ca2a1
unknown <tracer@hush.com>
2014-06-20 13:13:27 +0100

    TextBlob/NLTK based parsing. Better storage

---
+++ data.py
@@ -0,0 +1,32 @@
+import sqlite3
+import datetime
+import json
+
+class Storage(object):
+    def __init__(self, name):
+        d = datetime.datetime.utcnow().isoformat('-').replace(':', '')
+        d = d[:d.find('.')]  # microseconds? Er. Thanks. I'll pass
+        filename = 'osint_{0}_{1}.db'.format(name, d)
+        open(filename, 'a').close()
+        self.connection = sqlite3.connect(filename)
+        c = self.connection.cursor()
+        c.execute('CREATE TABLE profile (id INTEGER PRIMARY KEY, provider TEXT, query TEXT, name TEXT, data TEXT)')
+        c.execute('CREATE TABLE connection (parent_id INTEGER, child_id INTEGER, data TEXT)')
+        self.connection.commit()
+        
+    def __del__(self):
+        self.connection.close()
+        
+    def add_profile(self, provider, query, name, data):
+        params = (provider, query, name, json.dumps(data))
+        c = self.connection.cursor()
+        c.execute('INSERT INTO profile (provider, query, name, data) VALUES (?, ?, ?, ?)', params)
+        self.connection.commit()
+        return c.lastrowid
+        
+    def add_connection(self, parent_id, child_id, data):
+        params = (parent_id, child_id, data)
+        c = self.connection.cursor()
+        c.execute('INSERT INTO connection (parent_id, child_id, data) VALUES (?, ?, ?)', params)
+        self.connection.commit()
+
--- osint.py
+++ osint.py
@@ -1,17 +1,11 @@
 # osint.py
 
 import sys
-import datetime
 import json
-import sqlite3
+import data
 import twitter
 import reddit
 import whois
-
-def create_db(connection):
-    c = connection.cursor()
-    c.execute('CREATE TABLE dump (provider text, query_type text, data text)')
-    connection.commit()
 
 if __name__ == '__main__':
     config_file = open('config.json', 'r')
@@ -19,23 +13,12 @@ if __name__ == '__main__':
     config_file.close()
     
     target = sys.argv[1]
+    data = data.Storage(target)
 
     twitter = twitter.TwitterClient(config['twitter'])
     #reddit = reddit.RedditClient(config['reddit'])
-
-    d = datetime.datetime.utcnow().isoformat('-').replace(':', '')
-    d = d[:d.find('.')]  # microseconds? Er. Thanks. I'll pass
-    db_filename = '{0}-{1}.db'.format(target, d)
-    open(db_filename, 'a').close()
-    connection = sqlite3.connect(db_filename)
-    create_db(connection)
-
-    c = connection.cursor()
     
     # Twitter profile
     twitter_profile = twitter.get_profile(target)
-    c.execute('INSERT INTO dump (provider, query_type, data) VALUES (?, ?, ?)', ['twitter', 'profile', json.dumps(twitter_profile)])
-    connection.commit();
-    
-    connection.close()
+    print(data.add_profile('twitter', 'profile', target, twitter_profile))
     

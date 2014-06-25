2ddaf5290a075c56142c19892ed5d999651c88b8
unknown <tracer@hush.com>
2014-06-25 21:25:06 +0100

    Threading and queues

--- osint.py
+++ osint.py
@@ -2,10 +2,30 @@
 
 import sys
 import json
+import threading
 import data
+import Queue
 import twitter
 import reddit
 import whois
+
+def process(job_queue, clients, data):
+    while not job_queue.empty():
+        job = job_queue.get()
+        print(job)
+        result = clients[job['provider']].get_profile(job['target'])
+        data_queue.put({ 'provider': job['provider'], 'target': job['target'], 'data': result })
+        job_queue.task_done()
+    print('Stopping thread {0}'.format(threading.current_thread().name))
+        
+def process_data(data_queue):
+    d = data.Storage(target)
+    while(True):
+        result = data_queue.get()
+        print('d - {0}'.format(result))
+        d.add_profile(result['provider'], 'profile', result['target'], result['data'])
+        data_queue.task_done()
+        
 
 if __name__ == '__main__':
     config_file = open('config.json', 'r')
@@ -13,12 +33,29 @@ if __name__ == '__main__':
     config_file.close()
     
     target = sys.argv[1]
-    data = data.Storage(target)
 
     twitter = twitter.TwitterClient(config['twitter'])
     #reddit = reddit.RedditClient(config['reddit'])
     
-    # Twitter profile
-    twitter_profile = twitter.get_profile(target)
-    print(data.add_profile('twitter', 'profile', target, twitter_profile))
+    clients = { 'twitter': twitter }
+    
+    job_queue = Queue.Queue()
+    job_queue.put({ 'provider': 'twitter', 'task': 'profile', 'target': target })
+    data_queue = Queue.Queue()
+    
+    # 1 data thread
+    t = threading.Thread(name='data', target=process_data, args=[data_queue])
+    t.daemon = True
+    t.start()
+    
+    # 4 worker threads
+    for i in range(4):
+        t = threading.Thread(name=str(i), target=process, args=[job_queue, clients, data_queue])
+        t.daemon = True
+        t.start()
+        
+    job_queue.join()
+    data_queue.join()
+    
+    print('Done.')
     

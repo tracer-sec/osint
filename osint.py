e60b35f6dcdef867791b01257be605aeff27831a
unknown <tracer@hush.com>
2014-06-26 23:43:14 +0100

    Request limit. Clear queue when reached. Synchronous print

--- osint.py
+++ osint.py
@@ -10,7 +10,8 @@ import reddit
 import whois
 
 visited = []
-
+node_limit = 5
+    
 def get_job_key(job):
     return '{0}~{1}~{2}'.format(job['provider'], job['task'], job['target'])
 
@@ -21,30 +22,47 @@ def process(job_queue, clients, data):
             job_key = get_job_key(job)
             if job_key not in visited:
                 visited.append(job_key)
-                print(job)
+                print_s(job)
                 result = clients[job['provider']].get_profile(job['target'])
                 data_queue.put({ 'provider': job['provider'], 'target': job['target'], 'data': result })
-                print('Fetching connections')
+
                 connections = clients[job['provider']].get_connections(job['target'])
-                connections = []
                 for connection in connections:
-                    if job_key not in visited:
+                    connection_key = get_job_key(connection)
+                    if connection_key not in visited:
                         job_queue.put(connection)
         finally:
             job_queue.task_done()
+            
+        if len(visited) >= node_limit:
+            break
         
-    print('Stopping thread {0}'.format(threading.current_thread().name))
+    # Empty queue so it returns control to calling thread
+    while not job_queue.empty():
+        job_queue.get()
+        job_queue.task_done()
+    
+    print_s('Stopping thread')
         
         
 def process_data(data_queue):
     d = data.Storage(target)
     while(True):
         result = data_queue.get()
-        print('d - {0}'.format(result))
+        print_s('d - {0}'.format(result))
         d.add_profile(result['provider'], 'profile', result['target'], result['data'])
         data_queue.task_done()
         
-
+        
+write_lock = threading.Lock()
+        
+def print_s(s):
+    thread_id = threading.current_thread().name
+    write_lock.acquire()
+    print('[{0}] {1}'.format(thread_id, s))
+    write_lock.release()
+    
+    
 if __name__ == '__main__':
     config_file = open('config.json', 'r')
     config = json.load(config_file)
@@ -75,5 +93,5 @@ if __name__ == '__main__':
     job_queue.join()
     data_queue.join()
     
-    print('Done.')
+    print_s('Done.')
     
--- twitter.py
+++ twitter.py
@@ -33,7 +33,7 @@ class TwitterClient(object):
     def get_connections(self, screen_name):
         follower_data = self.get_followers(screen_name)
         followers = map(lambda x: { 'provider': 'twitter', 'task': 'profile', 'target': x }, follower_data['ids'])
-        print(followers)
+        #print(followers)
         result = followers
         return result
         

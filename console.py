1419f53ba789e2815496eb4f79815f0b278a8162
tracer-sec <tracer@hush.com>
2015-06-25 14:47:17 +0100

    Made console more resilient to crappy input

--- console.py
+++ console.py
@@ -2,7 +2,7 @@ import plugins
 import sys
 import data
 import model
-    
+
 plugins.load_all('config.json')
 
 target = sys.argv[1]
@@ -20,30 +20,43 @@ while True:
         
     elif tokens[0].lower() == 'list':
         # show list of nodes
-        if tokens[1].lower() == 'nodes':
-            nodes = d.get_nodes()
-            print('\n'.join(map(lambda x: str(x), nodes)))
-        elif tokens[1].lower() == 'actions':
-            actions = plugins.fetch_actions(tokens[2])
-            print('\n'.join(map(lambda x: str(x), actions)))
+        if len(tokens) > 1:
+            if tokens[1].lower() == 'nodes':
+                nodes = d.get_nodes()
+                print('\n'.join(map(lambda x: str(x), nodes)))
+            elif tokens[1].lower() == 'actions':
+                if len(tokens) > 2:
+                    actions = plugins.fetch_actions(tokens[2])
+                    print('\n'.join(map(lambda x: str(x), actions)))
+                else:
+                    print('USAGE: list actions NODE_TYPE')
+        else:
+            print('USAGE: list (nodes | actions NODE_TYPE)')
                 
     elif tokens[0].lower() == 'get':
-        id = int(tokens[1])
-        result = d.get_node(id)
-        print(result)
+        if len(tokens) > 1:
+            id = int(tokens[1])
+            result = d.get_node(id)
+            print(result)
+        else:
+            print('USAGE: get NODE_ID')
         
     elif tokens[0].lower() == 'run':
         # run plugin
-        action_name = tokens[1]
-        action = plugins.fetch(action_name)
-        target_id = int(tokens[2])
-        target = d.get_node(target_id)
-        result = action['func'](target)
-        for n in result:
-            d.add_node(n)
-            connection = model.Connection(target.id, n.id, action_name, 'concrete', '')
-            d.add_connection(connection)
-        print(result)
+        if len(tokens) > 2:
+            action_name = tokens[1]
+            action = plugins.fetch(action_name)
+            target_id = int(tokens[2])
+            target = d.get_node(target_id)
+            result = action['func'](target)
+            for n in result:
+                d.add_node(n)
+                connection = model.Connection(target.id, n.id, action_name, 'concrete', '')
+                d.add_connection(connection)
+            d.add_node(target)
+            print(result)
+        else:
+            print('USAGE: run ACTION NODE_ID')
         
     else:
         print('< Unknown command: ' + command)

76a0325ac457cdc2ef196f76ca361695f26c27b7
tracer-sec <tracer@hush.com>
2015-08-12 22:04:49 +0100

    Fix crash in reddit profile get if it couldn't find it

--- plugins/reddit.py
+++ plugins/reddit.py
@@ -12,7 +12,10 @@ class RedditClient(object):
         
     def get_profile(self, username):
         result = self.make_request('GET', '/user/{0}/about.json'.format(username), {})
-        return model.Node('reddit', result['data']['name'], result)
+        if 'data' in result and 'data' in result['data']:
+            return model.Node('reddit', result['data']['name'], result)
+        else:
+            return None
         
     def make_request(self, method, url, params):
         connection = httplib.HTTPSConnection('www.reddit.com')

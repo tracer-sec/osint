9c2c1a500a86769e95286064e4abc868c190df1e
tracer-sec <tracer@hush.com>
2015-08-16 22:48:59 +0100

    Action to collect HTTP headers for website nodes

--- plugins/web.py
+++ plugins/web.py
@@ -4,6 +4,18 @@ import urlparse
 import model
 import re
 
+BORING_HEADERS = [
+    'etag',
+    'set-cookie',
+    'content-length',
+    'content-type',
+    'date',
+    'expires',
+    'pragma',
+    'vary',
+    'cache-control'
+]
+
 def get_site_info(node):
     '''
     u = urlparse.urlparse(node.name)
@@ -11,9 +23,9 @@ def get_site_info(node):
         connection = httplib.HTTPConnection(u.netloc)
     elif u.scheme == 'https':
         connection = httplib.HTTPSConnection(u.netloc)
-        
+
     # Server header, meta-tags, etc.
-        
+
     connection.close()
     '''
     return []
@@ -42,6 +54,25 @@ def extract_administrative_emails(node):
             email_nodes.append(model.Node('email', email_address, {}))
     return email_nodes
     
+
+def get_http_server_headers(node):
+    # HEAD request to server
+    # Grab headers from response
+    u = urlparse.urlparse(node.name)
+    if u.scheme == 'http':
+        connection = httplib.HTTPConnection(u.netloc)
+    elif u.scheme == 'https':
+        connection = httplib.HTTPSConnection(u.netloc)
+        
+    connection.request('HEAD', '/')
+    response = connection.getresponse()
+    headers = response.getheaders()
+    connection.close()
+
+    # Filter out dull headers and add to the original node
+    node.data['headers'] = filter(lambda x: x[0] not in BORING_HEADERS, headers)
+    return []
+
     
 def get(config):
     return [
@@ -59,6 +90,11 @@ def get(config):
             'func': extract_administrative_emails,
             'name': 'Administrative contact',
             'acts_on': ['domain']
+        },
+        {
+            'func': get_http_server_headers,
+            'name': 'Get HTTP server headers',
+            'acts_on': ['website']
         }
     ]
     

b8fb08d7762f3c6e733fd196c9079a8f98564329
tracer-sec <tracer@hush.com>
2016-01-26 20:51:51 +0000

    More robust plugin loading

--- plugins.py
+++ plugins.py
@@ -9,37 +9,56 @@ client_list = {}
 current_path = os.path.dirname(os.path.abspath(__file__))
 
 def load_all(config_name):
-    config_name = os.path.join(current_path, config_name)
+    success = True
+    print('Loading config . . .')
+    try:
+        config_name = os.path.join(current_path, config_name)
 
-    config_file = open(config_name, 'r')
-    config = json.load(config_file)
-    config_file.close()
+        config_file = open(config_name, 'r')
+        config = json.load(config_file)
+        config_file.close()
+
+        print('Done')
+    except Exception as ex:
+        success = False
+        print(ex)
 
     print('Loading clients . . .')
     py_files = filter(lambda x: os.path.isfile(os.path.join(current_path, 'clients', x)) and x.lower().endswith('.py'), os.listdir(os.path.join(current_path, 'clients')))
     for py_file in py_files:
-        plugin = load(py_file, 'clients')
-        # TODO: dictionary lookup that returns None on failure?
-        if plugin.__name__ in config:
-            client = plugin.get(config[plugin.__name__])
-        else:
-            client = plugin.get(None)
-        print('* {0}'.format(plugin.__name__))
-        client_list[plugin.__name__] = client
+        try:
+            plugin = load(py_file, 'clients')
+            # TODO: dictionary lookup that returns None on failure?
+            if plugin.__name__ in config:
+                client = plugin.get(config[plugin.__name__])
+            else:
+                client = plugin.get(None)
+            client_list[plugin.__name__] = client
+            print('* {0}'.format(plugin.__name__))
+        except Exception as ex:
+            success = False
+            print(ex)
     print('Done')
 
     print('Loading plugins . . .')
     py_files = filter(lambda x: os.path.isfile(os.path.join(current_path, 'plugins', x)) and x.lower().endswith('.py'), os.listdir(os.path.join(current_path, 'plugins')))
     for py_file in py_files:
-        plugin = load(py_file, 'plugins')
-        # TODO: dictionary lookup that returns None on failure?
-        if plugin.__name__ in config:
-            actions = plugin.get(config[plugin.__name__])
-        else:
-            actions = plugin.get(None)
-        print('* {0} - ({1})'.format(plugin.__name__, len(actions)))
-        action_list.extend(actions)
+        try:
+            plugin = load(py_file, 'plugins')
+            # TODO: dictionary lookup that returns None on failure?
+            if plugin.__name__ in config:
+                actions = plugin.get(config[plugin.__name__])
+            else:
+                actions = plugin.get(None)
+            print('* {0} - ({1})'.format(plugin.__name__, len(actions)))
+            action_list.extend(actions)
+        except Exception as ex:
+            success = False
+            print(ex)
     print('Done')
+    
+    if not success:
+        print("*** some failures while loading!")
 
 def load(filename, subdir):
     full_path = os.path.join(current_path, subdir, filename)
@@ -65,6 +84,8 @@ def run(node, action_name):
         if action is not None:
             result = action['func'](node)
             for newNode in result:
+                # If we're missing node data, fill it if, assuming
+                # we have an appropriate client
                 if newNode.data is None:
                     if newNode.node_type in client_list:
                         client_list[newNode.node_type].get_data(newNode)
